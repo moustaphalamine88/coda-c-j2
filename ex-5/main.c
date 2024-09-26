a1f563afa3be8d04f17250f61f5e36e3d860480d
moustaphalamine <moustaphamhtlamine2@gmail.com>
2024-09-26 15:13:50 +0200

    exercice 5

---
+++ ex-5/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main()
+{
+	double nombre_1 = 12;
+	double nombre_2 = 7;
+	
+	double resultat;
+
+    resultat = (nombre_1 + nombre_2)/ 2;
+	printf("la moyenne de 12 et 7 est égale à %.2f\n", resultat);
+   
+	return 0;
+}
