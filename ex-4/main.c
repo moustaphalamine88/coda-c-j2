69bc8fb638333d7e0a1ea3552402a7c6a1466e9f
moustaphalamine <moustaphamhtlamine2@gmail.com>
2024-09-26 14:26:56 +0200

    exerce 4

---
+++ ex-4/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+   
+ int main()
+   {
+   	  
+   	  int number_1 = 3;
+   	  int number_2 = 9;
+   	  int resultat = number_1 * number_2;
+
+   	 printf("%d x %d = %d\n", number_1, number_2, resultat ); 
+   	return 0;
+   }
